4d93ef0775537c957a873f8e0d1abfe30dfffcd6
leoli <jafee201153@gmail.com>
2023-11-06 22:16:25 +0800

    feat: init project

---
+++ template.h
@@ -0,0 +1,34 @@
+/**
+ * @file template.h
+ * @author leoli (jafee201153@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-11-06
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef __${TM_FILENAME_BASE/(.*)/${1:/upcase}/}_H
+#define __${TM_FILENAME_BASE/(.*)/${1:/upcase}/}_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+
+/* Exported macro ------------------------------------------------------------*/
+
+/* Exported functions prototypes ---------------------------------------------*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __${TM_FILENAME_BASE/(.*)/${1:/upcase}/}_H
