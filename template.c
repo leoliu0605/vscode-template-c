40037d5603fa2c8d559039a412839039fd2b4121
Leo <jafee201153@gmail.com>
2025-07-15 13:56:15 +0800

    Update

--- template.c
+++ template.c
@@ -1,22 +1,8 @@
-/**
- * @file template.c
- * @author leoli (jafee201153@gmail.com)
- * @brief
- * @version 0.1
- * @date 2023-11-06
- *
- * @copyright Copyright (c) 2023
- *
- */
-
 #include "${TM_FILENAME_BASE}.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/*  */
-#pragma region includes
 
 #include <stdbool.h>
 #include <stddef.h>
@@ -25,42 +11,7 @@ extern "C" {
 #include <stdlib.h>
 #include <string.h>
 
-#pragma endregion includes
-
-/*  */
-#pragma region typedef
-
-#pragma endregion typedef
-
-/*  */
-#pragma region Private Function Prototypes
-
-#pragma endregion Private Function Prototypes
-
-/*  */
-#pragma region defines
-
-#pragma endregion defines
-
-/*  */
-#pragma region variables
-
-#pragma endregion variables
-
-/*  */
-#pragma region External Functions
-
-#pragma endregion External Functions
-
-/*  */
-#pragma region Exported Functions
-
-#pragma endregion Exported Functions
-
-/*  */
-#pragma region Private Functions
-
-#pragma endregion Private Functions
+$1
 
 #ifdef __cplusplus
 }
--- template.h
+++ template.h
@@ -1,23 +1,9 @@
-/**
- * @file template.h
- * @author leoli (jafee201153@gmail.com)
- * @brief
- * @version 0.1
- * @date 2023-11-06
- *
- * @copyright Copyright (c) 2023
- *
- */
-
-#ifndef __${TM_FILENAME_BASE / (.*) / ${1 : / upcase } / } _H
-#define __$ {TM_FILENAME_BASE / (.*) / ${1 : / upcase} / } _H
+#ifndef __${TM_FILENAME_BASE/(.*)/${1:/upcase}/}_H
+#define __${TM_FILENAME_BASE/(.*)/${1:/upcase}/}_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/*  */
-#pragma region includes
 
 #include <stdbool.h>
 #include <stddef.h>
@@ -26,22 +12,7 @@ extern "C" {
 #include <stdlib.h>
 #include <string.h>
 
-#pragma endregion includes
-
-/*  */
-#pragma region typedef
-
-#pragma endregion typedef
-
-/*  */
-#pragma region defines
-
-#pragma endregion defines
-
-/*  */
-#pragma region Exported Functions
-
-#pragma endregion Exported Functions
+$1
 
 #ifdef __cplusplus
 }
